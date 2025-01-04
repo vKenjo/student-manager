57c45bdf4978d2b1a545bfedeaa85bfe8ca7bbe2
Josh Kenn Viray <blasingkennviray@gmail.com>
2025-01-04 22:50:36 +0800

    line graphs for finances added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
 import React from 'react';
 
@@ -8,7 +9,7 @@ const AdminPage = () => {
 		<div className='p-4 flex gap-4 flex-col md:flex-row'>
 			{/* LEFT */}
 			<div className='w-full lg:w-2/3 flex flex-col gap-8'>
-				{/* USERCARD */}
+				{/* USER CARDS */}
 				<div className='flex gap-4 justify-between flex-wrap'>
 					<UserCard type='student' />
 					<UserCard type='teacher' />
@@ -17,17 +18,19 @@ const AdminPage = () => {
 				</div>
 				{/* MIDDLE CHARTS */}
 				<div className='flex gap-4 flex-col lg:flex-row'>
-          {/* COUNT CHART */}
-          <div className='w-full lg:w-1/3 h-[450px]'>
-            <CountChart />
-          </div>
-          {/* ATTENDANCE CHART */}
-          <div className='w-full lg:w-2/3 h-[450px]'>
-            <AttendanceChart />
-          </div>
-        </div>
+					{/* COUNT CHART */}
+					<div className='w-full lg:w-1/3 h-[450px]'>
+						<CountChart />
+					</div>
+					{/* ATTENDANCE CHART */}
+					<div className='w-full lg:w-2/3 h-[450px]'>
+						<AttendanceChart />
+					</div>
+				</div>
 				{/* BOTTOM CHARTS */}
-				<div className=''></div>
+				<div className="w-full h-[500px]">
+					<FinanceChart />
+				</div>
 			</div>
 
 			{/* RIGHT */}
--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -45,16 +45,23 @@ const data = [
 const AttendanceChart = () => {
 	return (
 		<div className='bg-white rounded-lg p-4 h-full'>
-			<div className='flex justify-between items-center'> 
+			<div className='flex justify-between items-center'>
 				<h1 className='text-lg font-semibold'>Attendance</h1>
 				<Image src='/moreDark.png' alt='' width={20} height={20} />
 			</div>
 			<ResponsiveContainer width='100%' height='90%'>
 				<BarChart width={500} height={300} data={data} barSize={20}>
 					<CartesianGrid strokeDasharray='3 3' vertical={false} stroke='#ddd' />
-					<XAxis dataKey='name' axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
-					<YAxis axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
-					<Tooltip contentStyle={{borderRadius:"10px", borderColor:"light-gray"}} />
+					<XAxis
+						dataKey='name'
+						axisLine={false}
+						tick={{ fill: '#d1d5db' }}
+						tickLine={false}
+					/>
+					<YAxis axisLine={false} tick={{ fill: '#d1d5db' }} tickLine={false} />
+					<Tooltip
+						contentStyle={{ borderRadius: '10px', borderColor: 'light-gray' }}
+					/>
 					<Legend
 						align='left'
 						verticalAlign='top'
