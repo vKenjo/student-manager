492d3aa7950ed22717f3e8ae9ced70b351e1e85d
Josh Kenn Viray <blasingkennviray@gmail.com>
2025-01-08 22:36:58 +0800

    teacersList page completed

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -56,8 +56,11 @@ const columns = [
 
 const TeachersListpage = () => {
 	const renderRow = (item: Teacher) => (
-		<tr>
-			<td>
+		<tr
+			key={item.id}
+			className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'
+		>
+			<td className='flex items-center gap-4 p-4'>
 				<Image
 					src={item.photo}
 					alt=''
