1491c028c906faa2751b9a10c66d752748edcc27
Josh Kenn Viray <blasingkennviray@gmail.com>
2025-01-08 22:38:27 +0800

    teacher portion completed

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,7 +14,7 @@ const Table = ({
 			<thead>
 				<tr className='text-left text-gray-500 text-sm'>
 					{columns.map((col) => (
-						<th key={col.accessor}>{col.header}</th>
+						<th key={col.accessor} className={col.className}>{col.header}</th>
 					))}
 				</tr>
 			</thead>
