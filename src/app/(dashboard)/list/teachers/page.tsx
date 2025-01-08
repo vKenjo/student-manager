41a3a76d8acd8234975d3308b3fa25b128ea17fe
Josh Kenn Viray <blasingkennviray@gmail.com>
2025-01-08 22:32:23 +0800

    table data initialized

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,9 +1,97 @@
 import React from 'react';
 import Image from 'next/image';
+import { role, teachersData } from '@/lib/data';
 import TableSearch from '@/components/TableSearch';
 import Pagination from '@/components/Pagination';
+import Link from 'next/link';
+import Table from '@/components/Table';
+
+type Teacher = {
+	id: number;
+	teacherId: string;
+	name: string;
+	email?: string;
+	photo: string;
+	phone: string;
+	subjects: string[];
+	classes: string[];
+	address: string;
+};
+
+const columns = [
+	{
+		header: 'Info',
+		accessor: 'info',
+	},
+	{
+		header: 'Teacher ID',
+		accessor: 'teacherId',
+		className: 'hidden md:table-cell',
+	},
+	{
+		header: 'Subjects',
+		accessor: 'subjects',
+		className: 'hidden md:table-cell',
+	},
+	{
+		header: 'Classes',
+		accessor: 'classes',
+		className: 'hidden md:table-cell',
+	},
+	{
+		header: 'Phone',
+		accessor: 'phone',
+		className: 'hidden lg:table-cell',
+	},
+	{
+		header: 'Address',
+		accessor: 'address',
+		className: 'hidden lg:table-cell',
+	},
+	{
+		header: 'Actions',
+		accessor: 'action',
+	},
+];
 
 const TeachersListpage = () => {
+	const renderRow = (item: Teacher) => (
+		<tr>
+			<td>
+				<Image
+					src={item.photo}
+					alt=''
+					width={40}
+					height={40}
+					className='md:hidden xl:block w-10 h-10 rounded-full object-cover'
+				/>
+				<div className='flex flex-col'>
+					<h3 className='font-semibold'>{item.name}</h3>
+					<p className='text-xs text-gray-500'>{item?.email}</p>
+				</div>
+			</td>
+			<td>{item.teacherId}</td>
+			<td>{item.subjects.join(',')}</td>
+			<td>{item.classes.join(',')}</td>
+			<td>{item.phone}</td>
+			<td>{item.address}</td>
+			<td>
+				<div className='flex items-center gap-2'>
+					<Link href={`/list/teachers/${item.id}`}>
+						<button className='w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky'>
+							<Image src='/view.png' alt='' width={16} height={16} />
+						</button>
+					</Link>
+					{role === 'admin' && (
+						<button className='w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky'>
+							<Image src='/view.png' alt='' width={16} height={16} />
+						</button>
+					)}
+				</div>
+			</td>
+		</tr>
+	);
+
 	return (
 		<div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
 			{/* TOP */}
@@ -25,6 +113,7 @@ const TeachersListpage = () => {
 				</div>
 			</div>
 			{/* LIST */}
+			<Table columns={columns} renderRow={renderRow} data={teachersData} />
 			{/* PAGINATION */}
 			<Pagination />
 		</div>
