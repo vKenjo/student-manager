7c51673e475e5d0b5c32299e321a249b31d3eaee
Josh Kenn Viray <blasingkennviray@gmail.com>
2025-01-09 22:57:33 +0800

    small table initialiez

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -15,6 +15,7 @@ const Table = ({
 				<tr className='text-left text-gray-500 text-sm'>
 					{columns.map((col) => (
 						<th key={col.accessor} className={col.className}>{col.header}</th>
+						ASD
 					))}
 				</tr>
 			</thead>
