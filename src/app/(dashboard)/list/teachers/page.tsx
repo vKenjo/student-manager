3cd55bd8a97f039d5f7e5b3ef1339dab057e6c54
Josh Kenn Viray <blasingkennviray@gmail.com>
2025-01-10 22:24:58 +0800

    announcements completed

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -109,9 +109,11 @@ const TeachersListpage = () => {
 						<button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
 							<Image src='/sort.png' alt='' width={14} height={14} />
 						</button>
-						<button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
-							<Image src='/plus.png' alt='' width={14} height={14} />
-						</button>
+						{role === 'admin' && (
+							<button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+								<Image src='/plus.png' alt='' width={14} height={14} />
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
